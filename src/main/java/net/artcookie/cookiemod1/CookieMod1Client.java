3266293f9c4906b40e011cb4799d474ef83c2afa
ArtFromCookieLand <mrfoxybas@gmail.com>
2023-07-15 17:30:29 +0300

    Initial commit

---
+++ src/main/java/net/artcookie/cookiemod1/CookieMod1Client.java
@@ -0,0 +1,10 @@
+package net.artcookie.cookiemod1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CookieMod1Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/artcookie/cookiemod1/CookieMod1DataGenerator.java
@@ -0,0 +1,11 @@
+package net.artcookie.cookiemod1;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CookieMod1DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
